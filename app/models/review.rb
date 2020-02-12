4d5d59ed4ecf7324a2e313ce73f75e8d8ce5ed93
Angel Shamieh <angellshamieh5@gmail.com>
2020-02-12 21:06:52 +0200

    Adding new model - review

---
+++ app/models/review.rb
@@ -0,0 +1,8 @@
+class Review < ApplicationRecord
+  belongs_to :cocktail
+
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :rating, numericality: { only_integer: true }
+end
